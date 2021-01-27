c6e18735ca3425479d07cb4e22a3c1490814077e
ararajs <aravindr002@e.ntu.edu.sg>
2021-01-27 16:35:57 +0800

    Added screen functionality - delegated tasks to Aravind, Su Qin and Jun Hao. Comments to identify who is to do what.

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,20 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
+  var msg = "";
+
+  void update(x) {
+    setState(() {
+      msg = "$msg$x";
+    });
+  }
+  void clear(){
+    setState(() {
+      msg = "";
+    });
+
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
@@ -28,29 +42,31 @@ class _HomePageState extends State<HomePage> {
           Expanded(
             flex: 2,
             child: Container(
-              color: Colors.red,
+              alignment: Alignment.bottomRight,
+              color: Colors.white,
+              child: Text("$msg", textScaleFactor: 2,)
             ),
           ),
 
           Expanded(
             flex: 2,
             child: Container(
-                  child: PageView(
-                    children: [
-                      Column(
-
-
-                      ),
-
-                      Column(
-
-
-                      )
-                    ],
+              color: Colors.yellow,
+              child: PageView(
+                children: [
+                  Column(
+                    //Jun Hao - trigo
                   ),
+                  Column(
+                    // Su Quin - log functions
+                  ),
+                  Column(
+                    // Aravind - logical operators
+                  )
+                ]
+              )
             ),
           ),
-
           Expanded(
             flex: 3,
             child: Container(
@@ -62,44 +78,44 @@ class _HomePageState extends State<HomePage> {
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget> [
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('7', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('8', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('9', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("7");},child: Text('7', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("8");},child: Text('8', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("9");},child: Text('9', textScaleFactor: 2,),)),
                       SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('DEL', textScaleFactor: 1.13,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('AC', textScaleFactor: 1.3,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { clear();},child: Text('AC', textScaleFactor: 1.3,),)),
                     ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget> [
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('4', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('5', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('6', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('x', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('/', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("4");},child: Text('4', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("5");},child: Text('5', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("6");},child: Text('6', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("x");},child: Text('x', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("/");},child: Text('/', textScaleFactor: 2,),)),
                     ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget> [
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('1', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('2', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('3', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('+', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('-', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("1");},child: Text('1', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("2");},child: Text('2', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("3");},child: Text('3', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("+");},child: Text('+', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("-");},child: Text('-', textScaleFactor: 2,),)),
                     ],
                   ),
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget> [
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('0', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('.', textScaleFactor: 2,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('x10^', textScaleFactor: 0.8,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("0");},child: Text('0', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update(".");},child: Text('.', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("x10^");},child: Text('x10^', textScaleFactor: 0.8,),)),
                       SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('ANS', textScaleFactor: 1,),)),
-                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { },child: Text('=', textScaleFactor: 2,),)),
+                      SizedBox(width: 65.0, height: 60.0,child: ElevatedButton(style: ElevatedButton.styleFrom(primary: Colors.white,onPrimary: Colors.black,), onPressed: () { update("=");},child: Text('=', textScaleFactor: 2,),)),
                     ],
                   ),
                 ],
